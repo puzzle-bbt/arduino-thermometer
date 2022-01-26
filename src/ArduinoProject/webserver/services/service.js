96532e1d636e22713759b8e6472bbd6f0c911e2b
Niklas Jäggi <jaeggi@puzzle.ch>
2022-01-26 16:26:38 +0100

    adding array values into table

--- src/ArduinoProject/webserver/services/service.js
+++ src/ArduinoProject/webserver/services/service.js
@@ -1,33 +1,8 @@
-/**
- * A synchron function example.
- * @param name the name of the person
- * @return {`Hello ${string}`} the hello message
- */
-exports.sayHello = (name) => {
-    console.log('say hello to ', name);
-    return `Hello ${name}`;
-}
-
-/**
- * A asynchron function example.
- * @param timeRange the time range like day, week or month
- * @return {Promise<string>} the congratulation message
- */
-exports.sayHelloAsync = async (timeRange) => {
-    return `I wish you a nice ${timeRange}.`
-}
-
-exports.createTable = (datas) => {
-    var table;
+exports.createTable = async (datas) => {
+    let text = "<ul>";
     for (var i = 0; i < datas.length; i++) {
-        table =
-        '<tr>' +
-        '<td>' +
-        datas[i] +
-        '</td>' +
-        '</tr>';
+        text += "<li>" + datas[i] + "</li>";
     }
-
-    return table;
-
+    text += "</ul>";
+    return text;
 }
