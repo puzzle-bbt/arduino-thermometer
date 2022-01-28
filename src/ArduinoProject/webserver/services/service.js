bd8ac5769c817809a56cb8ec393f6193778419ec
Lias <kleisa@puzzle.ch>
2022-01-28 07:38:43 +0100

    Add code for table in html and not only list

--- src/ArduinoProject/webserver/services/service.js
+++ src/ArduinoProject/webserver/services/service.js
@@ -1,8 +1,18 @@
-exports.createTable = async (datas) => {
+exports.createList = async (datas) => {
     let text = "<ul>";
     for (var i = 0; i < datas.length; i++) {
         text += "<li>" + datas[i] + "</li>";
     }
     text += "</ul>";
     return text;
+}
+
+exports.createTable = async (datas) => {
+    let table = "<table>";
+    let text = "<tr>";
+    for (var i = 0; i < datas.length; i++) {
+        text += "<td>" + datas[i] + "</td>" + "</tr>";
+    }
+    table += text + "</table>";
+    return table;
 }
